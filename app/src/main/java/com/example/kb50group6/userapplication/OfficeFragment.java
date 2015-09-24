16b1ee0734495da78a76f15808684c146a1c72f1
Robbert Eigenbrood <robberteigenbrood@gmail.com>
2015-09-24 10:40:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kb50group6/userapplication/OfficeFragment.java
@@ -0,0 +1,30 @@
+package com.example.kb50group6.userapplication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OfficeFragment extends Fragment {
+
+
+    public OfficeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_office, container, false);
+    }
+
+
+}
